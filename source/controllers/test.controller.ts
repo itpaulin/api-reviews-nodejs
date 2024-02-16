d6c14753b003cb50b18dab40e31a9c52d3f87f26
Paulo Ricardo <prmaltaleal@gmail.com>
2024-02-15 22:55:34 -0300

    feat: test function controller

---
+++ source/controllers/test.controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from 'express'
+import axios, { AxiosResponse } from 'axios'
+import { getEffectiveConstraintOfTypeParameter } from 'typescript'
+
+const testFunction = async (req:Request, res: Response, next: NextFunction) => {
+    let result: AxiosResponse = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    return res.send(result.data)
+}
+
+export default testFunction 
