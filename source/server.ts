400376fa197ba4f592495fddfa1c3f02f49c4521
Paulo Ricardo <prmaltaleal@gmail.com>
2024-02-18 03:50:36 -0300

    feat: restrict more and use secrets

--- source/server.ts
+++ source/server.ts
@@ -1,36 +1,41 @@
-import http from 'http'
-import  express, { Express } from 'express'
+import http from 'http';
+import express, { Express } from 'express';
 import morgan from 'morgan';
-import {router as routes} from './routes/index'
+import { router as routes } from './routes/index';
+import { PORT } from './secrets';
 
-const router: Express = express()
+const router: Express = express();
 
-router.use(morgan('dev'))
+router.use(morgan('dev'));
 
-router.use(express.urlencoded({ extended: false}))
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json());
+router.use(express.static('public'));
+router.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
 
-router.use(express.json())
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
+router.use('/', routes);
 
 router.use((req, res, next) => {
-    res.header(('Access-Control-Allow-Origin'), '*')	
-    res.header(('Access-Control-Allow-Headers'), 'origin, X-Requested-With, Content-Type, Accept, Authorization')
-
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
-        return res.status(200).json({})
-    }
-    next();
-})
-
-router.use('/', routes)
-
-router.use((req, res, next) => {
-    const error = new Error ('Not found');
-    return res.status(404).json({
-        message: error.message
-    })
-})
+  const error = new Error('Not found');
+  return res.status(404).json({
+    message: error.message
+  });
+});
 
 const httpServer = http.createServer(router);
-const PORT: any = process.env.PORT || 3000
-httpServer.listen(PORT, () => console.log(`the server is running on port ${PORT}`))
+const serverPort: number | string = PORT || 3000;
+httpServer.listen(serverPort, () =>
+  console.log(`Server is running on port ${serverPort}`)
+);
