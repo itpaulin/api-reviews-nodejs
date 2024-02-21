e2950da9e1d5d518512baf33546aed75c4ceace9
Paulo Ricardo <prmaltaleal@gmail.com>
2024-02-21 00:29:25 -0300

    feat: signup controller and router

--- source/controllers/auth.ts
+++ source/controllers/auth.ts
@@ -1,5 +1,25 @@
 import { Request, Response } from 'express';
+import { prismaClient } from '..';
+import { hashSync } from 'bcrypt';
 
-export const login = (req: Request, res: Response) => {
-  res.send('Login works');
+export const signup = async (req: Request, res: Response) => {
+  const { email, name, password, photoUrl } = req.body;
+
+  let user = await prismaClient.user.findFirst({ where: { email } });
+
+  if (user) {
+    throw Error('user already exists.');
+  }
+
+  user = await prismaClient.user.create({
+    data: {
+      name,
+      email,
+      password: hashSync(password, 10),
+      role: 'user',
+      photo: photoUrl ?? null
+    }
+  });
+  res.json(user);
 };
+// roles "user", "moderator" and "admin",
