fd4f5220ec8ed0d1bf942e68b0b809bff5c5289b
Paulo Ricardo <prmaltaleal@gmail.com>
2024-02-27 00:01:58 -0300

    feat: class unaunthorized error

---
+++ source/exceptions/unauthorized.ts
@@ -0,0 +1,7 @@
+import { ErrorCode, HttpExceptions } from './root';
+
+export class NotFoundException extends HttpExceptions {
+  constructor(message: string, errorCode: ErrorCode, errors?: any) {
+    super(message, errorCode, 401, errors);
+  }
+}
