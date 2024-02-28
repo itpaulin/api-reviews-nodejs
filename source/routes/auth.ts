e9e81e7cbc9b100af70d3c9a7dadc88b426c74e4
Paulo Ricardo <prmaltaleal@gmail.com>
2024-02-27 23:58:58 -0300

    feat: ensure requests use authmiddleware first

--- source/routes/auth.ts
+++ source/routes/auth.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
-import { login, signup } from '../controllers/auth';
+import { login, signup, me } from '../controllers/auth';
+import { errorHandler } from '../error-handler';
+import authMiddleware from '../middlewares/auth';
 
 const authRoutes: Router = Router();
 
-authRoutes.post('/signup', signup);
+authRoutes.post('/signup', errorHandler(signup));
+authRoutes.post('/login', errorHandler(login));
+authRoutes.get('/me', [authMiddleware], me);
 
-authRoutes.post('/login', login);
 export default authRoutes;
